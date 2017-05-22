2da7a429c1636c7ae1d6b6a4306ba985777fe975
nikolalsvk <nikolaseap@gmail.com>
2017-05-22 17:30:27 +0000

    Add meetings for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(:name => "nikolalsvk", :email => "nikolaseap@gmail.com")
-User.create(:name => "janavojnovic", :email => "jana_vojnovic@hotmail.com")
-User.create(:name => "marinanenic", :email => "marina_nenic@hotmail.com")
+nikola = User.create(:name => "nikolalsvk", :email => "nikolaseap@gmail.com")
+jana = User.create(:name => "janavojnovic", :email => "jana_vojnovic@hotmail.com")
+marina = User.create(:name => "marinanenic", :email => "marina_nenic@hotmail.com")
+
+nikola.meetings.create(:title => "Gym with Djoka",
+                       :description => "Get SWOLE")
+nikola.meetings.create(:title => "Running with Jana",
+                       :description => "Get RIPPED")
+
+jana.meetings.create(:title => "Movies with Branko",
+                     :description => "Watch Fast&Furious 7")
+jana.meetings.create(:title => "Gym with Maring",
+                     :description => "Get LEAN")
+
+marina.meetings.create(:title => "Chill",
+                       :description => "Just relax")
+marina.meetings.create(:title => "DOTA with Jana",
+                       :description => "Kill some creeps")
