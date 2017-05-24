b1b9499f52c447f52a21d247998b340379c7b616
nikolalsvk <nikolaseap@gmail.com>
2017-05-24 11:37:57 +0000

    Add seeds for intervals

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,38 @@ nikola = User.create(:name => "nikolalsvk", :email => "nikolaseap@gmail.com")
 jana = User.create(:name => "janavojnovic", :email => "jana_vojnovic@hotmail.com")
 marina = User.create(:name => "marinanenic", :email => "marina_nenic@hotmail.com")
 
-nikola.meetings.create(:title => "Gym with Djoka",
-                       :description => "Get SWOLE")
-nikola.meetings.create(:title => "Running with Jana",
-                       :description => "Get RIPPED")
+n_meeting_1 = nikola.meetings.create(:title => "Gym with Djoka",
+                                     :description => "Get SWOLE")
+n_meeting_2 = nikola.meetings.create(:title => "Running with Jana",
+                                     :description => "Get RIPPED")
 
-jana.meetings.create(:title => "Movies with Branko",
-                     :description => "Watch Fast&Furious 7")
-jana.meetings.create(:title => "Gym with Maring",
-                     :description => "Get LEAN")
+j_meeting_1 = jana.meetings.create(:title => "Movies with Branko",
+                                   :description => "Watch Fast&Furious 7")
+j_meeting_2 = jana.meetings.create(:title => "Gym with Marina",
+                                   :description => "Get LEAN")
 
-marina.meetings.create(:title => "Chill",
-                       :description => "Just relax")
-marina.meetings.create(:title => "DOTA with Jana",
-                       :description => "Kill some creeps")
+m_meeting_1 = marina.meetings.create(:title => "Chill",
+                                     :description => "Just relax")
+m_meeting_2 = marina.meetings.create(:title => "DOTA with Jana",
+                                     :description => "Kill some creeps")
+
+n_meeting_1.intervals.create(:start_time => DateTime.now,
+                             :end_time => DateTime.now + 2.hours,
+                             :user => nikola)
+n_meeting_2.intervals.create(:start_time => DateTime.now + 2.hours,
+                             :end_time => DateTime.now + 4.hours,
+                             :user => nikola)
+
+j_meeting_1.intervals.create(:start_time => DateTime.now,
+                             :end_time => DateTime.now + 2.hours,
+                             :user => jana)
+j_meeting_2.intervals.create(:start_time => DateTime.now + 2.hours,
+                             :end_time => DateTime.now + 4.hours,
+                             :user => jana)
+
+m_meeting_1.intervals.create(:start_time => DateTime.now,
+                             :end_time => DateTime.now + 2.hours,
+                             :user => marina)
+m_meeting_2.intervals.create(:start_time => DateTime.now + 2.hours,
+                             :end_time => DateTime.now + 4.hours,
+                             :user => marina)
