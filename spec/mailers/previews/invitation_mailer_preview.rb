cd033f55e1f8885e97333a074f7ae815b4a037bf
nikolalsvk <nikolaseap@gmail.com>
2017-09-02 19:10:12 +0000

    Implement mailer preview

--- spec/mailers/previews/invitation_mailer_preview.rb
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
-
+  def invitation_mailer
+    InvitationMailer.invitation_mail(Invitation.first)
+  end
 end
