bd952e3a42d1baa140de14f16b48fbf4eefe8453
nikolalsvk <nikolaseap@gmail.com>
2017-09-05 18:09:19 +0000

    Include meeting and location in the invitation email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,6 +2,7 @@ class InvitationMailer < ApplicationMailer
   def invitation_mail(invitation)
     @invitation = invitation
     @meeting = @invitation.meeting
+    @intervals = @meeting.intervals
     @user = @meeting.user
 
     mail(to: @invitation.email, subject: "#{@user.name} has invited you to a meeting!")
