36f8beef1f81575e2b112fa7d0d33dab85053bd2
nikolalsvk <nikolaseap@gmail.com>
2017-09-06 18:26:54 +0000

    Spec for create_by_user

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Invitation, :type => :model do
 
   it { should belong_to(:meeting) }
   it { should belong_to(:user) }
+
+  describe "#created_by_user" do
+    before do
+      @user = User.create
+      meeting = @user.meetings.create
+      @invitation = meeting.invitations.create
+    end
+
+    it "returns invitations created by user" do
+      expect(described_class.created_by_user(@user)).to eq([@invitation])
+    end
+  end
 end
