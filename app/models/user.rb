2b74702dc7e886ec912f330a969408e381e2ad17
nikolalsvk <nikolaseap@gmail.com>
2017-09-09 22:14:20 +0000

    Return user details after login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :invitations, :dependent => :destroy
   has_many :meetings_invited_to, :through => :invitations, :source => :meeting
   has_many :devices, :dependent => :destroy
+
+  def as_json(options = {})
+    super(options.merge({ except: [:password, :created_at, :updated_at] }))
+  end
 end
